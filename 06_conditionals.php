aba00f8eacfc4a0af1915b45f70ab9a6a8a96281
Yeasin Arafath <yeasin.arafath@bjitgroup.com>
2021-06-02 09:34:02 +0600

    Updating Conditional logic and switch statement

--- 06_conditionals.php
+++ 06_conditionals.php
@@ -5,20 +5,68 @@ $salary = 300000;
 
 // Sample if
 
+if ($age === 20) {
+    echo '1';
+}
+
 // Without circle braces
+
+if ($age === 20) echo '1';
 
 // Sample if-else
 
+if ($age > 20) {
+    echo '1';
+} else {
+    echo '2';
+}
+
 // Difference between == and ===
+
+// '==' is called equal and '===' is called identical.
+// The difference between '==' and '===' operator is that, '==' shpuld be used to check if the values of the two operands are equal or not.
+// On the other hand. '===' checks the values as well as the data type of operands.
 
 // if AND
 
+if ($age > 20 && $salary === 300000) {
+    echo 'Do Something';
+}
+
 // if OR
+
+if ($age > 20 || $salary === 300000) {
+    echo "Do Something";
+}
 
 // Ternary if
 
+echo $age > 20 ? "Young" : "Old";
+
 // Short ternary
+
+$myAge = $age ?: 18;
 
 // Null coalescing operator
 
+$myName = isset($name) ? $name : "Arafath";
+
+$myName = $name ?? "Arafath";
+
 // switch
+
+$userRole = "admin";
+
+switch ($userRole) {
+    case 'admin':
+        echo 'Admin';
+        break;
+    case 'editor':
+        echo "Editor";
+        break;
+    case 'user':
+        echo "User";
+        break;
+    default:
+        echo "Invalid role";
+}
