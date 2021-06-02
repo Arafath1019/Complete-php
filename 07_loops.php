5021d2881ed3eaf9ce93e4fee69c13629a4731bf
Yeasin Arafath <yeasin.arafath@bjitgroup.com>
2021-06-02 10:47:36 +0600

    Updating Loops

--- 07_loops.php
+++ 07_loops.php
@@ -1,13 +1,49 @@
 <?php
 
-// while
+// while loop
 
-// Loop with $counter
+$counter = 0;
+
+while ($counter < 10) {
+    echo $counter . "<br>";
+    $counter++;
+}
 
 // do - while
 
+do {
+    echo $counter . "<br>";
+    $counter++;
+} while ($counter < 10);
+
 // for
+
+for ($i = 0; $i < 10; $i++) {
+    echo $counter . "<br>";
+}
 
 // foreach
 
+$fruits = ['Banana', 'Apple', 'Orange'];
+
+foreach ($fruits as $index => $fruit) {
+    echo $index . ' ' . $fruit . "<br>";
+}
+
+
 // Iterate Over associative array.
+
+$person = [
+    'name' => 'Yeasin Arafath',
+    'surname' => 'Arafath',
+    'age' => 23,
+    'hobbies' => ['Video Games', 'Travelling']
+];
+
+foreach ($person as $key => $value) {
+    if (is_array($value)) {
+        echo $key . ' ' . implode(",", $value) . "<br>";
+    } else {
+        echo $key . ' ' . $value . "<br>";
+    }
+}
