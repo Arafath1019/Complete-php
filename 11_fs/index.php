1294d266b00a65d5383a948194bafb5dd489ccbf
Yeasin Arafath <yeasin.arafath@bjitgroup.com>
2021-06-06 01:33:04 +0600

    Updating working with file system

--- 11_fs/index.php
+++ 11_fs/index.php
@@ -1,20 +1,34 @@
 <?php
 // Magic constants
+echo __DIR__ . "<br>";
+echo __FILE__ . "<br>";
+echo __LINE__ . "<br>";
 
 // Create directory
+mkdir('test');
 
 // Rename directory
+rename('test', 'test2');
 
 // Delete directory
+rmdir('test2');
 
 // Read files and folders inside directory
+echo scandir('./');
 
 // file_get_contents, file_put_contents
+echo file_get_contents('lorem.txt');
+
+file_put_contents('sample.txt', 'some content');
 
 // file_get_contents from URL
+$userJson = file_get_contents('https://jsonplaceholder.typicode.com/users');
+echo json_decode($userJson);
 
 // https://www.php.net/manual/en/book.filesystem.php
 // file_exists
+echo file_exists('sample.txt');
+echo is_dir('test');
 // is_dir
 // filemtime
 // filesize
