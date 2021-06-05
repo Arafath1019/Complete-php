5b1039630bf69cf2dd133ae3b6336b87412f9bd7
Yeasin Arafath <yeasin.arafath@bjitgroup.com>
2021-06-06 02:03:20 +0600

    Updating OOP

--- 12_oop/index.php
+++ 12_oop/index.php
@@ -7,3 +7,62 @@
 // Create instance of Person
 
 // Using setter and getter
+
+
+class Fruit
+{
+    public $name;
+    private $color;
+    protected $weight;
+    static $numbers;
+
+    public function __construct($name, $color, $weight, $numbers)
+    {
+        $this->name = $name;
+        $this->color = $color;
+        $this->weight = $weight;
+        $this->$numbers;
+    }
+
+    public function set_color($color)
+    {
+        $this->color = $color;
+    }
+
+    public function get_color()
+    {
+        return $this->color;
+    }
+
+    public function hello()
+    {
+        echo "Hello from parent class";
+    }
+}
+
+class Strawberry extends Fruit
+{
+    public $amount;
+
+    public function __construct($name, $color, $weight, $numbers, $amount)
+    {
+        parent::__construct($name, $color, $weight, $numbers);
+        $this->amount = $amount;
+    }
+
+    public function msg()
+    {
+        echo "Hello from child class";
+    }
+
+    static function newMsg()
+    {
+        echo "this is static method";
+    }
+}
+
+Strawberry::newMsg();
+
+$strawberry1 = new Strawberry("name", "red", 5, 10, 30);
+
+$strawberry1->msg();
